2a9de141d41c29fc088e9de51dc13f95eca47f26
moreiragonzalo <umoreiragonzalo@gmail.com>
2023-09-16 15:47:34 -0300

    Console.WriteLine modified

--- TestGitHub/Program.cs
+++ TestGitHub/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, GitHub!");
+
+            Console.ReadKey();
         }
     }
 }

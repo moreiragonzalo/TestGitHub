243fd71578c7262b99f7f94a302557af2b3a700c
moreiragonzalo <umoreiragonzalo@gmail.com>
2023-09-16 16:26:05 -0300

    Revert "Keeping one message"
    This reverts commit 595ec704bfe6d75e5db4485284b98fe43c249d21.

--- TestGitHub/Program.cs
+++ TestGitHub/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, GitHub! One more modification"); 
+            Console.WriteLine("Hello, GitHub! One more modification"); Console.WriteLine("Hello, GitHub! One more modification"); Console.WriteLine("Hello, GitHub! One more modification");Console.WriteLine("Hello, GitHub! One more modification"); Console.WriteLine("Hello, GitHub! One more modification"); Console.WriteLine("Hello, GitHub! One more modification"); Console.WriteLine("Hello, GitHub! One more modification");
+
 
             Console.ReadKey();
         }
